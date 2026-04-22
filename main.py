ca8fb46376966004d372558f6d38accb9683cace
AmyJeong123 <ajsoftware@skku.edu>
2026-04-22 18:32:46 +0900

    implement even_list function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def even_list(int_list: List[int]) -> List[int]:
     """
     Determines if a number is even and return an even list.
     """
-    pass
+    return [num for num in int_list if num % 2 == 0]
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     """
